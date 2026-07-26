387ec3e95d86610f25a3260a20bd9cd0473f2863
K-agya <149212228+K-agya@users.noreply.github.com>
2026-07-26 01:34:08 +0100

    Refactor courses view to use AppScaffold

--- lib/views/courses_view.dart
+++ lib/views/courses_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:moodle/widgets/nav_drawer.dart';
+import 'package:moodle/widgets/app_scaffold.dart';
 import 'package:moodle/constants.dart';
 
 class CoursesView extends StatelessWidget {
@@ -7,59 +7,8 @@ class CoursesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: moodleWhite,
-        foregroundColor: moodleTextDark,
-        elevation: 1,
-        titleSpacing: 0,
-        title: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 16),
-                width: 32,
-                height: 32,
-                child: Image.asset(
-                  'images/moodle_logo.png',
-                  fit: BoxFit.contain,
-                ),
-              ),
-              const Text(
-                'My courses',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              ),
-            ],
-          ),
-        ),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search_outlined),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.notifications_none_outlined),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.chat_bubble_outline),
-            onPressed: () {},
-          ),
-          const SizedBox(width: 8),
-          const CircleAvatar(
-            radius: 18,
-            backgroundColor: moodleGrayBg,
-            foregroundColor: moodlePurple,
-            child: Text(
-              'YH',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
-            ),
-          ),
-          const SizedBox(width: 16),
-        ],
-      ),
-      drawer: const NavDrawer(),
+    return AppScaffold(
+      title: 'My courses',
       body: Container(
         color: moodleBg,
         child: const SingleChildScrollView(
